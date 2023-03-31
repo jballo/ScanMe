984837821e33879f3961cc2edaa79cfb91f2c08f
Jonathan Ballona <jballona5127@gmail.com>
2023-03-30 17:13:37 -0700

    Basic UI Needed For Functionality

--- ScanMe/ScanMe/ViewController.swift
+++ ScanMe/ScanMe/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var SelectPhotoBttn: UIButton!
+    
+    
+    @IBOutlet weak var GetTxtBttn: UIButton!
+    
+    
+    @IBOutlet weak var ConvertedTxt: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func SelectPhoto(_ sender: Any) {
+        print("We will take a photo and display it")
+    }
+    @IBAction func ConvertText(_ sender: Any) {
+        print("We will grab the text from the image and display it")
+    }
+    
 
 }
 
